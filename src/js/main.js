d13864ba450be1319f0b9f59df4b6c3250626e61
MMeskhi <mishameskhi1@gmail.com>
2023-02-27 12:51:33 +0400

    add text animation and padding fixes

--- src/js/main.js
+++ src/js/main.js
@@ -66,6 +66,21 @@ window.onscroll = function (event) {
   }
 };
 
+//Hero slider text animation
+const textWrapper = document.querySelector(".ml3");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+anime.timeline({ loop: false }).add({
+  targets: ".ml3 .letter",
+  opacity: [0, 1],
+  easing: "easeInOutQuad",
+  duration: 100,
+  delay: (el, i) => 150 * (i + 1),
+});
+
 //Sliders
 const swiper = new Swiper(".main-hero .mySwiper", {
   autoplay: {
